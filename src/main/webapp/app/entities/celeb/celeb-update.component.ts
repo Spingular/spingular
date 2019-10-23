930dc96c40c78215585ed67c248ab23f45132500
Tonterias <ecorreos@htomail.com>
2019-10-23 21:52:46 +0200

    Celeb Entity commit

--- src/main/webapp/app/entities/celeb/celeb-update.component.ts
+++ src/main/webapp/app/entities/celeb/celeb-update.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { JhiAlertService } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 import { ICeleb, Celeb } from 'app/shared/model/celeb.model';
 import { CelebService } from './celeb.service';
 import { IAppuser } from 'app/shared/model/appuser.model';
 import { AppuserService } from 'app/entities/appuser/appuser.service';
+import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-celeb-update',
@@ -20,6 +21,28 @@ export class CelebUpdateComponent implements OnInit {
   isSaving: boolean;
 
   appusers: IAppuser[];
+
+  celeb: ICeleb;
+  celebs: ICeleb[];
+
+  isCreateDisabled = false;
+
+  nameParamUprofileId: any;
+  valueParamUprofileId: any;
+
+  currentAccount: any;
+  currentSearch: string;
+  routeData: any;
+  links: any;
+  totalItems: any;
+  itemsPerPage: any;
+  page: any = 1;
+  predicate: any = 'id';
+  previousPage: any = 0;
+  reverse: any = 'asc';
+  id: any;
+  owner: any;
+  isAdmin: boolean;
 
   editForm = this.fb.group({
     id: [],
@@ -32,21 +55,175 @@ export class CelebUpdateComponent implements OnInit {
     protected celebService: CelebService,
     protected appuserService: AppuserService,
     protected activatedRoute: ActivatedRoute,
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    protected router: Router,
+    protected parseLinks: JhiParseLinks,
+    protected accountService: AccountService,
+    protected eventManager: JhiEventManager
+  ) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params.uprofileIdEquals != null) {
+        this.nameParamUprofileId = 'uprofile.userId';
+        this.valueParamUprofileId = params.uprofileIdEquals;
+      }
+    });
+  }
 
   ngOnInit() {
     this.isSaving = false;
     this.activatedRoute.data.subscribe(({ celeb }) => {
       this.updateForm(celeb);
+      this.celeb = celeb;
     });
-    this.appuserService
-      .query()
-      .pipe(
-        filter((mayBeOk: HttpResponse<IAppuser[]>) => mayBeOk.ok),
-        map((response: HttpResponse<IAppuser[]>) => response.body)
-      )
-      .subscribe((res: IAppuser[]) => (this.appusers = res), (res: HttpErrorResponse) => this.onError(res.message));
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+          this.appusers = res.body;
+          this.myUserCelebs(this.owner);
+          // this.loggedUser = res.body[0];
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  private myUserCelebs(owner) {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['appuserId.equals'] = this.owner;
+    }
+    this.celebService.query(query).subscribe(
+      (res: HttpResponse<ICeleb[]>) => {
+        this.celebs = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  loadAll() {
+    if (this.currentSearch) {
+      this.celebService
+        .query({
+          page: this.page - 1,
+          'celebName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
+    this.celebService
+      .query({
+        page: this.page - 1,
+        size: this.itemsPerPage,
+        sort: this.sort()
+      })
+      .subscribe(
+        (res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  addExistingProfileCeleb(celebId) {
+    this.isSaving = true;
+    if (celebId !== undefined) {
+      const query = {};
+      query['id.equals'] = celebId;
+      this.celebService.query(query).subscribe(
+        (res: HttpResponse<ICeleb[]>) => {
+          this.celebs = res.body;
+          const query2 = {};
+          if (this.valueParamUprofileId != null) {
+            query2['id.equals'] = this.valueParamUprofileId;
+          }
+          this.appuserService.query(query2).subscribe(
+            (res2: HttpResponse<IAppuser[]>) => {
+              this.celebs[0].appusers.push(res2.body[0]);
+              this.subscribeToSaveResponse(this.celebService.update(this.celebs[0]));
+            },
+            (res2: HttpErrorResponse) => this.onError(res2.message)
+          );
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    }
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.transition();
+    }
+  }
+
+  transition() {
+    this.router.navigate(['/celeb/new'], {
+      queryParams: {
+        page: this.page,
+        size: this.itemsPerPage,
+        search: this.currentSearch,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    });
+    this.loadAll();
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  search(query) {
+    this.isCreateDisabled = true;
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
+    this.router.navigate([
+      '/celeb/new',
+      {
+        search: this.currentSearch,
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  clear() {
+    this.page = 0;
+    this.currentSearch = '';
+    this.router.navigate([
+      '/celeb/new',
+      {
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  private paginateCelebs(data: ICeleb[], headers: HttpHeaders) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.celebs = data;
+    if (this.totalItems === 0) {
+      this.celeb.celebName = this.currentSearch;
+    }
   }
 
   updateForm(celeb: ICeleb) {
--- src/main/webapp/app/entities/celeb/celeb.component.ts
+++ src/main/webapp/app/entities/celeb/celeb.component.ts
@@ -1,16 +1,144 @@
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { HttpHeaders, HttpResponse } from '@angular/common/http';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { Subscription } from 'rxjs';
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import { filter, map } from 'rxjs/operators';
+// import { JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+
+// import { ICeleb } from 'app/shared/model/celeb.model';
+// import { AccountService } from 'app/core/auth/account.service';
+
+// import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+// import { CelebService } from './celeb.service';
+
+// @Component({
+//   selector: 'jhi-celeb',
+//   templateUrl: './celeb.component.html'
+// })
+// export class CelebComponent implements OnInit, OnDestroy {
+//   currentAccount: any;
+//   celebs: ICeleb[];
+//   error: any;
+//   success: any;
+//   eventSubscriber: Subscription;
+//   routeData: any;
+//   links: any;
+//   totalItems: any;
+//   itemsPerPage: any;
+//   page: any;
+//   predicate: any;
+//   previousPage: any;
+//   reverse: any;
+
+//   constructor(
+//     protected celebService: CelebService,
+//     protected parseLinks: JhiParseLinks,
+//     protected accountService: AccountService,
+//     protected activatedRoute: ActivatedRoute,
+//     protected router: Router,
+//     protected eventManager: JhiEventManager
+//   ) {
+//     this.itemsPerPage = ITEMS_PER_PAGE;
+//     this.routeData = this.activatedRoute.data.subscribe(data => {
+//       this.page = data.pagingParams.page;
+//       this.previousPage = data.pagingParams.page;
+//       this.reverse = data.pagingParams.ascending;
+//       this.predicate = data.pagingParams.predicate;
+//     });
+//   }
+
+//   loadAll() {
+//     this.celebService
+//       .query({
+//         page: this.page - 1,
+//         size: this.itemsPerPage,
+//         sort: this.sort()
+//       })
+//       .subscribe((res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers));
+//   }
+
+//   loadPage(page: number) {
+//     if (page !== this.previousPage) {
+//       this.previousPage = page;
+//       this.transition();
+//     }
+//   }
+
+//   transition() {
+//     this.router.navigate(['/celeb'], {
+//       queryParams: {
+//         page: this.page,
+//         size: this.itemsPerPage,
+//         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+//       }
+//     });
+//     this.loadAll();
+//   }
+
+//   clear() {
+//     this.page = 0;
+//     this.router.navigate([
+//       '/celeb',
+//       {
+//         page: this.page,
+//         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+//       }
+//     ]);
+//     this.loadAll();
+//   }
+
+//   ngOnInit() {
+//     this.loadAll();
+//     this.accountService.identity().subscribe(account => {
+//       this.currentAccount = account;
+//     });
+//     this.registerChangeInCelebs();
+//   }
+
+//   ngOnDestroy() {
+//     this.eventManager.destroy(this.eventSubscriber);
+//   }
+
+//   trackId(index: number, item: ICeleb) {
+//     return item.id;
+//   }
+
+//   registerChangeInCelebs() {
+//     this.eventSubscriber = this.eventManager.subscribe('celebListModification', response => this.loadAll());
+//   }
+
+//   sort() {
+//     const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+//     if (this.predicate !== 'id') {
+//       result.push('id');
+//     }
+//     return result;
+//   }
+
+//   protected paginateCelebs(data: ICeleb[], headers: HttpHeaders) {
+//     this.links = this.parseLinks.parse(headers.get('link'));
+//     this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+//     this.celebs = data;
+//   }
+// }
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { filter, map } from 'rxjs/operators';
-import { JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
 import { ICeleb } from 'app/shared/model/celeb.model';
+import { CelebService } from './celeb.service';
+
+import { IAppuser } from 'app/shared/model/appuser.model';
+import { AppuserService } from 'app/entities/appuser/appuser.service';
 import { AccountService } from 'app/core/auth/account.service';
 
 import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
-import { CelebService } from './celeb.service';
 
 @Component({
   selector: 'jhi-celeb',
@@ -31,13 +159,20 @@ export class CelebComponent implements OnInit, OnDestroy {
   previousPage: any;
   reverse: any;
 
+  appusers: IAppuser[];
+  currentSearch: string;
+  owner: any;
+  isAdmin: boolean;
+
   constructor(
     protected celebService: CelebService,
     protected parseLinks: JhiParseLinks,
     protected accountService: AccountService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
-    protected eventManager: JhiEventManager
+    protected eventManager: JhiEventManager,
+    protected appuserService: AppuserService,
+    protected jhiAlertService: JhiAlertService
   ) {
     this.itemsPerPage = ITEMS_PER_PAGE;
     this.routeData = this.activatedRoute.data.subscribe(data => {
@@ -46,16 +181,35 @@ export class CelebComponent implements OnInit, OnDestroy {
       this.reverse = data.pagingParams.ascending;
       this.predicate = data.pagingParams.predicate;
     });
+    this.currentSearch =
+      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
   }
 
   loadAll() {
+    if (this.currentSearch) {
+      this.celebService
+        .query({
+          page: this.page - 1,
+          'celebName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
     this.celebService
       .query({
         page: this.page - 1,
         size: this.itemsPerPage,
         sort: this.sort()
       })
-      .subscribe((res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers));
+      .subscribe(
+        (res: HttpResponse<ICeleb[]>) => this.paginateCelebs(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
   }
 
   loadPage(page: number) {
@@ -70,6 +224,7 @@ export class CelebComponent implements OnInit, OnDestroy {
       queryParams: {
         page: this.page,
         size: this.itemsPerPage,
+        search: this.currentSearch,
         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
       }
     });
@@ -78,6 +233,7 @@ export class CelebComponent implements OnInit, OnDestroy {
 
   clear() {
     this.page = 0;
+    this.currentSearch = '';
     this.router.navigate([
       '/celeb',
       {
@@ -88,12 +244,53 @@ export class CelebComponent implements OnInit, OnDestroy {
     this.loadAll();
   }
 
+  search(query: string) {
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
+    this.router.navigate([
+      '/celeb',
+      {
+        search: this.currentSearch,
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
   ngOnInit() {
     this.loadAll();
-    this.accountService.identity().subscribe(account => {
-      this.currentAccount = account;
-    });
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
     this.registerChangeInCelebs();
+  }
+
+  myCelebs() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['appuserId.equals'] = this.owner;
+    }
+    this.celebService.query(query).subscribe(
+      (res: HttpResponse<ICeleb[]>) => {
+        this.celebs = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
   }
 
   ngOnDestroy() {
@@ -120,5 +317,9 @@ export class CelebComponent implements OnInit, OnDestroy {
     this.links = this.parseLinks.parse(headers.get('link'));
     this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
     this.celebs = data;
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
   }
 }
