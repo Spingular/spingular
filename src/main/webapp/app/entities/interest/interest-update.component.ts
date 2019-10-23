e078d513cfce8c73ed311cdda8afaa7dec464e36
Tonterias <ecorreos@htomail.com>
2019-10-23 18:39:01 +0200

    Interest Entity commit

--- src/main/webapp/app/entities/interest/interest-update.component.ts
+++ src/main/webapp/app/entities/interest/interest-update.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { JhiAlertService } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 import { IInterest, Interest } from 'app/shared/model/interest.model';
 import { InterestService } from './interest.service';
 import { IAppuser } from 'app/shared/model/appuser.model';
 import { AppuserService } from 'app/entities/appuser/appuser.service';
+import { AccountService } from 'app/core/auth/account.service';
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
 
 @Component({
   selector: 'jhi-interest-update',
@@ -20,6 +22,28 @@ export class InterestUpdateComponent implements OnInit {
   isSaving: boolean;
 
   appusers: IAppuser[];
+
+  interest: IInterest;
+  interests: IInterest[];
+
+  isCreateDisabled = false;
+
+  nameParamUprofileId: any;
+  valueParamUprofileId: any;
+
+  currentAccount: any;
+  currentSearch: string;
+  routeData: any;
+  links: any;
+  totalItems: any;
+  itemsPerPage: any;
+  page: any = 1;
+  predicate: any = 'id';
+  previousPage: any = 0;
+  reverse: any = 'asc';
+  id: any;
+  owner: any;
+  isAdmin: boolean;
 
   editForm = this.fb.group({
     id: [],
@@ -32,21 +56,189 @@ export class InterestUpdateComponent implements OnInit {
     protected interestService: InterestService,
     protected appuserService: AppuserService,
     protected activatedRoute: ActivatedRoute,
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    protected router: Router,
+    protected parseLinks: JhiParseLinks,
+    protected accountService: AccountService,
+    protected eventManager: JhiEventManager
+  ) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params.uprofileIdEquals != null) {
+        this.nameParamUprofileId = 'uprofile.userId';
+        this.valueParamUprofileId = params.uprofileIdEquals;
+      }
+    });
+  }
+
+  // ngOnInit() {
+  //   this.isSaving = false;
+  //   this.activatedRoute.data.subscribe(({ interest }) => {
+  //     this.updateForm(interest);
+  //   });
+  //   this.appuserService
+  //     .query()
+  //     .pipe(
+  //       filter((mayBeOk: HttpResponse<IAppuser[]>) => mayBeOk.ok),
+  //       map((response: HttpResponse<IAppuser[]>) => response.body)
+  //     )
+  //     .subscribe((res: IAppuser[]) => (this.appusers = res), (res: HttpErrorResponse) => this.onError(res.message));
+  // }
 
   ngOnInit() {
     this.isSaving = false;
     this.activatedRoute.data.subscribe(({ interest }) => {
       this.updateForm(interest);
+      this.interest = interest;
     });
-    this.appuserService
-      .query()
-      .pipe(
-        filter((mayBeOk: HttpResponse<IAppuser[]>) => mayBeOk.ok),
-        map((response: HttpResponse<IAppuser[]>) => response.body)
-      )
-      .subscribe((res: IAppuser[]) => (this.appusers = res), (res: HttpErrorResponse) => this.onError(res.message));
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+          this.appusers = res.body;
+          this.myUserInterests(this.owner);
+          // this.loggedUser = res.body[0];
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  private myUserInterests(owner) {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['appuserId.equals'] = this.owner;
+    }
+    this.interestService.query(query).subscribe(
+      (res: HttpResponse<IInterest[]>) => {
+        this.interests = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  loadAll() {
+    if (this.currentSearch) {
+      this.interestService
+        .query({
+          page: this.page - 1,
+          'interestName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
+    this.interestService
+      .query({
+        page: this.page - 1,
+        size: this.itemsPerPage,
+        sort: this.sort()
+      })
+      .subscribe(
+        (res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  addExistingProfileInterest(interestId) {
+    this.isSaving = true;
+    if (interestId !== undefined) {
+      const query = {};
+      query['id.equals'] = interestId;
+      this.interestService.query(query).subscribe(
+        (res: HttpResponse<IInterest[]>) => {
+          this.interests = res.body;
+          const query2 = {};
+          if (this.valueParamUprofileId != null) {
+            query2['id.equals'] = this.valueParamUprofileId;
+          }
+          this.appuserService.query(query2).subscribe(
+            (res2: HttpResponse<IAppuser[]>) => {
+              this.interests[0].appusers.push(res2.body[0]);
+              this.subscribeToSaveResponse(this.interestService.update(this.interests[0]));
+            },
+            (res2: HttpErrorResponse) => this.onError(res2.message)
+          );
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    }
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.transition();
+    }
+  }
+
+  transition() {
+    this.router.navigate(['/interest/new'], {
+      queryParams: {
+        page: this.page,
+        size: this.itemsPerPage,
+        search: this.currentSearch,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    });
+    this.loadAll();
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  search(query) {
+    this.isCreateDisabled = true;
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
+    this.router.navigate([
+      '/interest/new',
+      {
+        search: this.currentSearch,
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  clear() {
+    this.page = 0;
+    this.currentSearch = '';
+    this.router.navigate([
+      '/interest/new',
+      {
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  private paginateInterests(data: IInterest[], headers: HttpHeaders) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.interests = data;
+    if (this.totalItems === 0) {
+      this.interest.interestName = this.currentSearch;
+    }
   }
 
   updateForm(interest: IInterest) {
--- src/main/webapp/app/entities/interest/interest.component.ts
+++ src/main/webapp/app/entities/interest/interest.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { filter, map } from 'rxjs/operators';
-import { JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
 import { IInterest } from 'app/shared/model/interest.model';
+import { InterestService } from './interest.service';
+
+import { IAppuser } from 'app/shared/model/appuser.model';
+import { AppuserService } from 'app/entities/appuser/appuser.service';
 import { AccountService } from 'app/core/auth/account.service';
 
 import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
-import { InterestService } from './interest.service';
 
 @Component({
   selector: 'jhi-interest',
@@ -31,13 +34,20 @@ export class InterestComponent implements OnInit, OnDestroy {
   previousPage: any;
   reverse: any;
 
+  appusers: IAppuser[];
+  currentSearch: string;
+  owner: any;
+  isAdmin: boolean;
+
   constructor(
     protected interestService: InterestService,
     protected parseLinks: JhiParseLinks,
     protected accountService: AccountService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
-    protected eventManager: JhiEventManager
+    protected eventManager: JhiEventManager,
+    protected appuserService: AppuserService,
+    protected jhiAlertService: JhiAlertService
   ) {
     this.itemsPerPage = ITEMS_PER_PAGE;
     this.routeData = this.activatedRoute.data.subscribe(data => {
@@ -46,16 +56,45 @@ export class InterestComponent implements OnInit, OnDestroy {
       this.reverse = data.pagingParams.ascending;
       this.predicate = data.pagingParams.predicate;
     });
+    this.currentSearch =
+      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
   }
 
+  // loadAll() {
+  //   this.interestService
+  //     .query({
+  //       page: this.page - 1,
+  //       size: this.itemsPerPage,
+  //       sort: this.sort()
+  //     })
+  //     .subscribe((res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers));
+  // }
+
   loadAll() {
+    if (this.currentSearch) {
+      this.interestService
+        .query({
+          page: this.page - 1,
+          'interestName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
     this.interestService
       .query({
         page: this.page - 1,
         size: this.itemsPerPage,
         sort: this.sort()
       })
-      .subscribe((res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers));
+      .subscribe(
+        (res: HttpResponse<IInterest[]>) => this.paginateInterests(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
   }
 
   loadPage(page: number) {
@@ -70,6 +109,7 @@ export class InterestComponent implements OnInit, OnDestroy {
       queryParams: {
         page: this.page,
         size: this.itemsPerPage,
+        search: this.currentSearch,
         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
       }
     });
@@ -78,9 +118,27 @@ export class InterestComponent implements OnInit, OnDestroy {
 
   clear() {
     this.page = 0;
+    search: this.currentSearch,
+      this.router.navigate([
+        '/interest',
+        {
+          page: this.page,
+          sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+        }
+      ]);
+    this.loadAll();
+  }
+
+  search(query) {
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
     this.router.navigate([
       '/interest',
       {
+        search: this.currentSearch,
         page: this.page,
         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
       }
@@ -88,13 +146,64 @@ export class InterestComponent implements OnInit, OnDestroy {
     this.loadAll();
   }
 
+  // ngOnInit() {
+  //   this.loadAll();
+  //   this.accountService.identity().subscribe(account => {
+  //     this.currentAccount = account;
+  //   });
+  //   this.registerChangeInInterests();
+  // }
+
   ngOnInit() {
     this.loadAll();
-    this.accountService.identity().subscribe(account => {
-      this.currentAccount = account;
-    });
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+          // this.loggedUser = res.body[0];
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
     this.registerChangeInInterests();
   }
+
+  myInterests() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['appuserId.equals'] = this.owner;
+    }
+    this.interestService.query(query).subscribe(
+      (res: HttpResponse<IInterest[]>) => {
+        this.interests = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  // private uprofileInterests() {
+  //     const query = {};
+  //     if (this.uprofiles != null) {
+  //         const arrayUprofiles = [];
+  //         this.uprofiles.forEach(uprofile => {
+  //             arrayUprofiles.push(uprofile.id);
+  //         });
+  //         query['uprofileId.in'] = arrayUprofiles;
+  //     }
+  //     this.interestService.query(query).subscribe(
+  //         (res: HttpResponse<IInterest[]>) => {
+  //             this.interests = res.body;
+  //             //                console.log('CONSOLOG: M:communitiesActivities & O: res.body : ', res.body);
+  //         },
+  //         (res: HttpErrorResponse) => this.onError(res.message)
+  //     );
+  // }
 
   ngOnDestroy() {
     this.eventManager.destroy(this.eventSubscriber);
@@ -120,5 +229,9 @@ export class InterestComponent implements OnInit, OnDestroy {
     this.links = this.parseLinks.parse(headers.get('link'));
     this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
     this.interests = data;
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
   }
 }
