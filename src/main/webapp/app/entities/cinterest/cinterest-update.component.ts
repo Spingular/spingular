e5d555dd909ba9927fdec4771efc4fe4bbcfd92f
Tonterias <ecorreos@htomail.com>
2019-10-24 12:19:31 +0200

    CInterest +celeb+interest fix Entity commit

--- src/main/webapp/app/entities/cinterest/cinterest-update.component.ts
+++ src/main/webapp/app/entities/cinterest/cinterest-update.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { JhiAlertService } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 import { ICinterest, Cinterest } from 'app/shared/model/cinterest.model';
 import { CinterestService } from './cinterest.service';
 import { ICommunity } from 'app/shared/model/community.model';
 import { CommunityService } from 'app/entities/community/community.service';
+
+import { IAppuser } from 'app/shared/model/appuser.model';
+import { AppuserService } from 'app/entities/appuser/appuser.service';
+import { AccountService } from 'app/core/auth/account.service';
+
+// import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
 
 @Component({
   selector: 'jhi-cinterest-update',
@@ -20,6 +26,31 @@ export class CinterestUpdateComponent implements OnInit {
   isSaving: boolean;
 
   communities: ICommunity[];
+  arrayCommmunities = [];
+
+  cinterests: ICinterest[];
+  cinterest: ICinterest;
+  appusers: IAppuser[];
+  appuser: IAppuser;
+  isCreateDisabled = false;
+
+  nameParamCommunityId: any;
+  valueParamCommunityId: any;
+
+  owner: any;
+  isAdmin: boolean;
+
+  currentAccount: any;
+  currentSearch: string;
+  routeData: any;
+  links: any;
+  totalItems: any;
+  itemsPerPage: any;
+  page: any = 1;
+  predicate: any = 'id';
+  previousPage: any = 0;
+  reverse: any = 'asc';
+  id: any;
 
   editForm = this.fb.group({
     id: [],
@@ -32,21 +63,162 @@ export class CinterestUpdateComponent implements OnInit {
     protected cinterestService: CinterestService,
     protected communityService: CommunityService,
     protected activatedRoute: ActivatedRoute,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    protected parseLinks: JhiParseLinks,
+    protected appuserService: AppuserService,
+    protected accountService: AccountService,
+    protected router: Router,
+    protected eventManager: JhiEventManager
   ) {}
 
   ngOnInit() {
     this.isSaving = false;
     this.activatedRoute.data.subscribe(({ cinterest }) => {
-      this.updateForm(cinterest);
+      this.cinterest = cinterest;
     });
-    this.communityService
-      .query()
-      .pipe(
-        filter((mayBeOk: HttpResponse<ICommunity[]>) => mayBeOk.ok),
-        map((response: HttpResponse<ICommunity[]>) => response.body)
-      )
-      .subscribe((res: ICommunity[]) => (this.communities = res), (res: HttpErrorResponse) => this.onError(res.message));
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+          const query2 = {};
+          if (this.currentAccount.id != null) {
+            query2['appuserId.equals'] = this.owner;
+          }
+          this.communityService.query(query2).subscribe(
+            (res2: HttpResponse<ICommunity[]>) => {
+              this.communities = res2.body;
+            },
+            (res2: HttpErrorResponse) => this.onError(res2.message)
+          );
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  loadAll() {
+    if (this.currentSearch) {
+      this.cinterestService
+        .query({
+          page: this.page - 1,
+          'interestName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<ICinterest[]>) => this.paginateCinterests(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
+    this.cinterestService
+      .query({
+        page: this.page - 1,
+        size: this.itemsPerPage,
+        sort: this.sort()
+      })
+      .subscribe(
+        (res: HttpResponse<ICinterest[]>) => this.paginateCinterests(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  addExistingProfileCinterest(cinterestId) {
+    this.isSaving = true;
+    if (cinterestId !== undefined) {
+      const query = {};
+      query['id.equals'] = cinterestId;
+      this.cinterestService.query(query).subscribe(
+        (res: HttpResponse<ICinterest[]>) => {
+          this.cinterests = res.body;
+          const query2 = {};
+          if (this.valueParamCommunityId != null) {
+            query2['id.equals'] = this.valueParamCommunityId;
+          }
+          this.communityService.query(query2).subscribe(
+            (res2: HttpResponse<ICommunity[]>) => {
+              this.cinterests[0].communities.push(res2.body[0]);
+              this.subscribeToSaveResponse(this.cinterestService.update(this.cinterests[0]));
+            },
+            (res2: HttpErrorResponse) => this.onError(res2.message)
+          );
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    }
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.transition();
+    }
+  }
+
+  transition() {
+    this.router.navigate(['/cinterest/new'], {
+      queryParams: {
+        page: this.page,
+        size: this.itemsPerPage,
+        search: this.currentSearch,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    });
+    this.loadAll();
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  search(query) {
+    this.isCreateDisabled = true;
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
+    this.router.navigate([
+      '/cinterest/new',
+      {
+        search: this.currentSearch,
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  clear() {
+    this.page = 0;
+    this.currentSearch = '';
+    this.router.navigate([
+      '/cinterest/new',
+      {
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
+  private paginateCinterests(data: ICinterest[], headers: HttpHeaders) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.cinterests = data;
+    if (this.totalItems === 0) {
+      this.cinterest.interestName = this.currentSearch;
+    }
   }
 
   updateForm(cinterest: ICinterest) {
@@ -97,6 +269,10 @@ export class CinterestUpdateComponent implements OnInit {
   }
 
   trackCommunityById(index: number, item: ICommunity) {
+    return item.id;
+  }
+
+  trackId(index: number, item: ICinterest) {
     return item.id;
   }
 
--- src/main/webapp/app/entities/cinterest/cinterest.component.ts
+++ src/main/webapp/app/entities/cinterest/cinterest.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { filter, map } from 'rxjs/operators';
-import { JhiEventManager, JhiParseLinks } from 'ng-jhipster';
+// import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
 import { ICinterest } from 'app/shared/model/cinterest.model';
+import { CinterestService } from './cinterest.service';
+import { ICommunity } from 'app/shared/model/community.model';
+import { CommunityService } from 'app/entities/community/community.service';
+
+import { IAppuser } from 'app/shared/model/appuser.model';
+import { AppuserService } from 'app/entities/appuser/appuser.service';
 import { AccountService } from 'app/core/auth/account.service';
 
 import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
-import { CinterestService } from './cinterest.service';
 
 @Component({
   selector: 'jhi-cinterest',
@@ -31,13 +36,23 @@ export class CinterestComponent implements OnInit, OnDestroy {
   previousPage: any;
   reverse: any;
 
+  appusers: IAppuser[];
+  appuser: IAppuser;
+  communities: ICommunity[];
+  owner: any;
+  isAdmin: boolean;
+  currentSearch: string;
+
   constructor(
     protected cinterestService: CinterestService,
     protected parseLinks: JhiParseLinks,
     protected accountService: AccountService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
-    protected eventManager: JhiEventManager
+    protected eventManager: JhiEventManager,
+    protected communityService: CommunityService,
+    protected appuserService: AppuserService,
+    protected jhiAlertService: JhiAlertService
   ) {
     this.itemsPerPage = ITEMS_PER_PAGE;
     this.routeData = this.activatedRoute.data.subscribe(data => {
@@ -46,16 +61,35 @@ export class CinterestComponent implements OnInit, OnDestroy {
       this.reverse = data.pagingParams.ascending;
       this.predicate = data.pagingParams.predicate;
     });
+    this.currentSearch =
+      this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search'] ? this.activatedRoute.snapshot.params['search'] : '';
   }
 
   loadAll() {
+    if (this.currentSearch) {
+      this.cinterestService
+        .query({
+          page: this.page - 1,
+          'interestName.contains': this.currentSearch,
+          size: this.itemsPerPage,
+          sort: this.sort()
+        })
+        .subscribe(
+          (res: HttpResponse<ICinterest[]>) => this.paginateCinterests(res.body, res.headers),
+          (res: HttpErrorResponse) => this.onError(res.message)
+        );
+      return;
+    }
     this.cinterestService
       .query({
         page: this.page - 1,
         size: this.itemsPerPage,
         sort: this.sort()
       })
-      .subscribe((res: HttpResponse<ICinterest[]>) => this.paginateCinterests(res.body, res.headers));
+      .subscribe(
+        (res: HttpResponse<ICinterest[]>) => this.paginateCinterests(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
   }
 
   loadPage(page: number) {
@@ -70,6 +104,7 @@ export class CinterestComponent implements OnInit, OnDestroy {
       queryParams: {
         page: this.page,
         size: this.itemsPerPage,
+        search: this.currentSearch,
         sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
       }
     });
@@ -78,6 +113,7 @@ export class CinterestComponent implements OnInit, OnDestroy {
 
   clear() {
     this.page = 0;
+    this.currentSearch = '';
     this.router.navigate([
       '/cinterest',
       {
@@ -88,12 +124,72 @@ export class CinterestComponent implements OnInit, OnDestroy {
     this.loadAll();
   }
 
+  search(query) {
+    if (!query) {
+      return this.clear();
+    }
+    this.page = 0;
+    this.currentSearch = query;
+    this.router.navigate([
+      '/cinterest',
+      {
+        search: this.currentSearch,
+        page: this.page,
+        sort: this.predicate + ',' + (this.reverse ? 'asc' : 'desc')
+      }
+    ]);
+    this.loadAll();
+  }
+
   ngOnInit() {
     this.loadAll();
-    this.accountService.identity().subscribe(account => {
-      this.currentAccount = account;
-    });
+    this.accountService.identity().subscribe(
+      account => {
+        this.currentAccount = account;
+        this.isAdmin = this.accountService.hasAnyAuthority(['ROLE_ADMIN']);
+        const query = {};
+        if (this.currentAccount.id != null) {
+          query['userId.equals'] = this.currentAccount.id;
+        }
+        this.appuserService.query(query).subscribe((res: HttpResponse<IAppuser[]>) => {
+          this.owner = res.body[0].id;
+          // this.loggedUser = res.body[0];
+        });
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
     this.registerChangeInCinterests();
+  }
+
+  myCinterests() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['appuserId.equals'] = this.owner;
+    }
+    this.communityService.query(query).subscribe(
+      (res: HttpResponse<ICommunity[]>) => {
+        this.communities = res.body;
+        this.communityCinterests();
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
+  }
+
+  private communityCinterests() {
+    const query = {};
+    if (this.communities != null) {
+      const arrayCommunities = [];
+      this.communities.forEach(community => {
+        arrayCommunities.push(community.id);
+      });
+      query['communityId.in'] = arrayCommunities;
+    }
+    this.cinterestService.query(query).subscribe(
+      (res: HttpResponse<ICommunity[]>) => {
+        this.cinterests = res.body;
+      },
+      (res: HttpErrorResponse) => this.onError(res.message)
+    );
   }
 
   ngOnDestroy() {
@@ -120,5 +216,9 @@ export class CinterestComponent implements OnInit, OnDestroy {
     this.links = this.parseLinks.parse(headers.get('link'));
     this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
     this.cinterests = data;
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
   }
 }
