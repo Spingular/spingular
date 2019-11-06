040a9b2d19615e96d8862c5682967ea45e16657c
Mike <miguel7suarez7@gmail.com>
2019-11-06 09:32:17 +0100

    NavBar notifications icons commit

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -10,6 +10,17 @@ import { AccountService } from 'app/core/auth/account.service';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { LoginService } from 'app/core/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+
+import { INotification } from 'app/shared/model/notification.model';
+import { NotificationService } from '../.././../app/entities/notification/notification.service';
+import { IMessage } from 'app/shared/model/message.model';
+import { MessageService } from '../.././../app/entities/message/message.service';
+import { ICmessage } from 'app/shared/model/cmessage.model';
+import { CmessageService } from '../.././../app/entities/cmessage/cmessage.service';
+import { ICommunity } from 'app/shared/model/community.model';
+import { CommunityService } from '../.././../app/entities/community/community.service';
 
 @Component({
   selector: 'jhi-navbar',
@@ -22,7 +33,19 @@ export class NavbarComponent implements OnInit {
   languages: any[];
   swaggerEnabled: boolean;
   modalRef: NgbModalRef;
+
   version: string;
+  currentAccount: any;
+  loginName: string;
+
+  numberOfNotifications: number;
+  numberOfMessages: number;
+  numberOfCmessages: number;
+  numberOfCommunities: number;
+
+  messages: IMessage[];
+  cmessages: ICmessage[];
+  communities: ICommunity[];
 
   constructor(
     private loginService: LoginService,
@@ -32,7 +55,12 @@ export class NavbarComponent implements OnInit {
     private accountService: AccountService,
     private loginModalService: LoginModalService,
     private profileService: ProfileService,
-    private router: Router
+    private router: Router,
+    private jhiAlertService: JhiAlertService,
+    private notificationService: NotificationService,
+    private communityService: CommunityService,
+    private messageService: MessageService,
+    private cmessageService: CmessageService
   ) {
     this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
     this.isNavbarCollapsed = true;
@@ -45,6 +73,92 @@ export class NavbarComponent implements OnInit {
       this.inProduction = profileInfo.inProduction;
       this.swaggerEnabled = profileInfo.swaggerEnabled;
     });
+    this.accountService.getAuthenticationState().subscribe(state => {
+      if (state) {
+        this.loginService.editLoginStatus(true);
+      }
+    });
+    this.loginService.loginCast.subscribe(status => {
+      if (status) {
+        this.loginData();
+      }
+    });
+  }
+
+  loginData() {
+    this.accountService.identity().then(account => {
+      this.currentAccount = account;
+      this.loginName = this.currentAccount.login;
+      this.notifications().subscribe(
+        (res: HttpResponse<INotification[]>) => {
+          this.numberOfNotifications = res.body.length;
+          return this.numberOfNotifications;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+      this.mymessages().subscribe(
+        (res: HttpResponse<IMessage[]>) => {
+          this.numberOfMessages = res.body.length;
+          return this.numberOfMessages;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+      this.myMessagesCommunities().subscribe(
+        (res2: HttpResponse<ICommunity[]>) => {
+          this.communities = res2.body;
+          this.communitiesMessages().subscribe(
+            (res3: HttpResponse<ICmessage[]>) => {
+              this.numberOfCmessages = res3.body.length;
+            },
+            (res3: HttpErrorResponse) => this.onError(res3.message)
+          );
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    });
+  }
+
+  private notifications() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['userId.equals'] = this.currentAccount.id;
+      query['isDelivered.equals'] = 'false';
+    }
+    return this.notificationService.query(query);
+  }
+
+  private mymessages() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['receiverId.equals'] = this.currentAccount.id;
+      query['isDelivered.equals'] = 'false';
+    }
+    return this.messageService.query(query);
+  }
+
+  private myMessagesCommunities() {
+    const query = {};
+    if (this.currentAccount.id != null) {
+      query['userId.equals'] = this.currentAccount.id;
+    }
+    return this.communityService.query(query);
+  }
+
+  private communitiesMessages() {
+    const query = {};
+    if (this.communities != null) {
+      const arrayCommmunities = [];
+      this.communities.forEach(community => {
+        arrayCommmunities.push(community.id);
+      });
+      query['creceiverId.in'] = arrayCommmunities;
+      query['isDelivered.equals'] = 'false';
+    }
+    return this.cmessageService.query(query);
+  }
+
+  private onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
   }
 
   changeLanguage(languageKey: string) {
